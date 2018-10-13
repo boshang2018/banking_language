250b59cd9d93280ba9a6d249fc7aba7bb68dbc3e
Steffen Zschaler <steffen.zschaler@kcl.ac.uk>
2018-10-13 17:00:31 +0100

    Even fuller banking example

--- uk.ac.kcl.inf.modelling.banking.design/src/uk/ac/kcl/inf/modelling/banking/design/Services.java
+++ uk.ac.kcl.inf.modelling.banking.design/src/uk/ac/kcl/inf/modelling/banking/design/Services.java
@@ -1,11 +1,16 @@
 package uk.ac.kcl.inf.modelling.banking.design;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gemoc.executionframework.extensions.sirius.services.AbstractGemocAnimatorServices;
+import org.eclipse.gemoc.executionframework.extensions.sirius.services.AbstractGemocAnimatorServices.StringCouple;
 
 /**
  * The services class used by VSM.
  */
-public class Services {
+public class Services extends AbstractGemocAnimatorServices  {
     
     /**
     * See http://help.eclipse.org/neon/index.jsp?topic=%2Forg.eclipse.sirius.doc%2Fdoc%2Findex.html&cp=24 for documentation on how to write service methods.
@@ -14,4 +19,11 @@ public class Services {
        // TODO Auto-generated code
       return self;
     }
+
+	@Override
+	protected List<StringCouple> getRepresentationRefreshList() {
+		final List<StringCouple> res = new ArrayList<StringCouple>();
+		res.add(new StringCouple("Banking Diagram", "Default"));
+		return res;
+	}
 }
